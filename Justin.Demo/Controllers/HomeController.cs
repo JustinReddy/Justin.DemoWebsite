c36b266a9574492e00738384f3138757a8fd6c85
Justin Reddy <reddy.justin@gmail.com>
2018-07-22 19:34:57 +0200

    inital commit
    initial commit website shell with 2 statics pages

---
+++ Justin.Demo/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Justin.Demo.Models;
+
+namespace Justin.Demo.Controllers
+{
+    public class HomeController : Controller
+    {
+        /// <summary>
+        /// https://github.com/nax3t/background-slider
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Welcome()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Justin.Demo/wwwroot/js/site.js
@@ -0,0 +1,23 @@
+﻿// Write your JavaScript code.
+
+// https://jsfiddle.net/MrMavin/0gop9xp9/
+document.addEventListener('DOMContentLoaded', function () {
+    var body = document.body,
+        html = document.documentElement;
+
+    function getHeightInPixels() {
+        return Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight) + 'px';
+    }
+
+    $(window).on('resize', function () {
+        $('#particles-js').css({
+            height: getHeightInPixels
+        });
+    });
+
+    particlesJS("particles-js", { "particles": { "number": { "value": 13, "density": { "enable": true, "value_area": 1000 } }, "color": { "value": "#ffffff" }, "shape": { "type": "image", "stroke": { "width": 0, "color": "#000000" }, "polygon": { "nb_sides": 5 }, "image": { "src": "https://gdurl.com/0VzA", "width": 100, "height": 100 } }, "opacity": { "value": 0.5, "random": false, "anim": { "enable": false, "speed": 1, "opacity_min": 0.1, "sync": false } }, "size": { "value": 41.6, "random": true, "anim": { "enable": false, "speed": 40, "size_min": 0.1, "sync": false } }, "line_linked": { "enable": false, "distance": 150, "color": "#ffffff", "opacity": 0.4, "width": 1 }, "move": { "enable": true, "speed": 0.6, "direction": "top", "random": false, "straight": false, "out_mode": "out", "bounce": false, "attract": { "enable": false, "rotateX": 600, "rotateY": 1200 } } }, "interactivity": { "detect_on": "window", "events": { "onhover": { "enable": false, "mode": "grab" }, "onclick": { "enable": false, "mode": "push" }, "resize": true }, "modes": { "grab": { "distance": 400, "line_linked": { "opacity": 1 } }, "bubble": { "distance": 400, "size": 40, "duration": 2, "opacity": 8, "speed": 3 }, "repulse": { "distance": 200, "duration": 0.4 }, "push": { "particles_nb": 4 }, "remove": { "particles_nb": 2 } } }, "retina_detect": true });
+
+    $('#particles-js').css({
+        height: getHeightInPixels()
+    });
+});
